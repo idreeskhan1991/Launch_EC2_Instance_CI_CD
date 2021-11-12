34f5cc5bc47f924ebd9eda74ad6d229827f6a498
idreeskhan1991 <92720694+idreeskhan1991@users.noreply.github.com>
2021-11-12 21:15:59 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
-
+# Terraform state will be stored in S3
+terraform {
+  backend "s3" {
+    bucket = "lunch-ec2-cicd"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 # Use AWS Terraform provider
 provider "aws" {
   region = "us-east-1"

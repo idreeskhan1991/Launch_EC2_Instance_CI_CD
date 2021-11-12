345cfa7827571e350b5389788c4509ba6756bb43
idreeskhan1991 <92720694+idreeskhan1991@users.noreply.github.com>
2021-11-12 20:15:12 +0530

    Updated

--- main.tf
+++ main.tf
@@ -16,31 +16,11 @@ provider "aws" {
 resource "aws_instance" "default" {
   ami                    = var.ami
   count                  = var.instance_count
-  vpc_security_group_ids = [aws_security_group.default.id]
+  vpc_security_group_ids = "terraform-default-sg"
   source_dest_check      = false
   instance_type          = var.instance_type
 
   tags = {
     Name = "terraform-default"
   }
-}
-
-# Create Security Group for EC2
-resource "aws_security_group" "default" {
-  name = "terraform-default-sg"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
 }

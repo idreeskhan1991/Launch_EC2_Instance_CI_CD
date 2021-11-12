2fd5cb1189dedf856f4ccbfa7c9d6e9673f8e413
idreeskhan1991 <92720694+idreeskhan1991@users.noreply.github.com>
2021-11-12 17:35:52 +0530

    New  Files Added

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "instance_count" {
+  default = 1
+}
+
+variable "instance_type" {
+  description = "AWS instance type"
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "Base AMI to launch the instances"
+
+  # Bitnami NGINX AMI
+  default = "ami-0416f96ae3d1a3f29"
+}
